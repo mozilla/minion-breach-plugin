6b57bb5606b731acefda33b9b27d0c7e08ffdf04
Yeuk Hon Wong <yeukhon@mozilla.com>
2013-08-12 15:01:55 +0000

    Initialized breach plugin package structure.

---
+++ setup.py
@@ -0,0 +1,20 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from setuptools import setup
+
+install_requires = [
+    'minion-backend'
+]
+
+setup(name="minion-breach-plugin",
+      version="0.1",
+      description="BREACH Plugin for Minion",
+      url="https://github.com/mozilla/minion-breach-plugin/",
+      author="Mozilla",
+      author_email="minion@mozilla.com",
+      packages=['minion', 'minion.plugins'],
+      namespace_packages=['minion', 'minion.plugins'],
+      include_package_data=True,
+      install_requires = install_requires)
