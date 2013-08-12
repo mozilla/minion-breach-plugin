c2ee1dda82e7d346b2223382b48d6a36b64e3c78
Yeuk Hon Wong <yeukhon@mozilla.com>
2013-08-12 20:56:26 +0000

    Try HTTPs destination on behalf of the user.

--- minion/plugins/breach.py
+++ minion/plugins/breach.py
@@ -2,9 +2,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import minion.curly
+import urlparse
 
-from minion.plugins.base import BlockingPlugin
+import minion.curly
+from minion.plugins.base import AbstractPlugin, BlockingPlugin
 
 class BreachPlugin(BlockingPlugin):
     """
@@ -37,10 +38,14 @@ class BreachPlugin(BlockingPlugin):
         }
     ]
 
+    HTTP_SUBMITTED = "The target URL submitted is HTTP. BREACH is an attack \
+against any protocol over TLS/SSL. Instead, Minion automatically tried the HTTPS \
+version."
+
     REPORTS = {
-        "good": 
+        "disabled": 
             {
-                "Summary": "HTTP compression is not enabled",
+                "Summary": "HTTPS site is not vulnerable to BREACH attack",
                 "Description": 'A HTTP request was sent out to the server with \
 "Accept-Encoding: compress, gzip, deflate, bzip2, lzma" in the request header. \
 The server responded with no HTTP compression.',
@@ -48,37 +53,72 @@ The server responded with no HTTP compression.',
                 "URLs": [ {"URL": None, "Extra": None}],
                 "FurtherInfo": FURTHER_INFO
             },
-        "bad": 
+        "enabled": 
             {
-                "Summary": "HTTP compression is enabled",
+                "Summary": "HTTPS site is vulnerable to BREACH attack",
                 "Description": 'A HTTP request was sent out to the server with \
 "Accept-Encoding: compress, gzip, deflate, bzip2, lzma" in the request header. \
-The server replied with {header} in the response header',
+The server replied with {header} in the response header.',
                 "Severity": "High",
                 "URLs": [ {"URL": None, "Extra": None}],
+                "FurtherInfo": FURTHER_INFO
+            },
+        "netural":
+            {
+                "Summary": "HTTP site is not vulnerable to BREACH attack",
+                "Description": "The BREACH attack is not applicable on HTTP site.",
+                "Severity": "Info",
+                "URLs": [{"URL": None, "Extra": None}],
                 "FurtherInfo": FURTHER_INFO
             }
 }
 
     def do_run(self):
         try:
+            url_list = None
+            was_http = False
+            target = self.configuration['target']
+            url = urlparse.urlparse(target)
+            if url.scheme == 'http':
+                target = target.replace('http', 'https')
+                was_http = True
+                url_list = [{'URL': target, 'Extra': self.HTTP_SUBMITTED}]
+
             r = minion.curly.get(
-                self.configuration['target'], 
+                target, 
                 connect_timeout=5, 
                 timeout=15,
                 headers={'accept-encoding': 'compress, gzip, deflate, bzip2, lzma'})
             r.raise_for_status()
+
+            # If HTTP compression is enabled
             if "content-encoding" in r.headers:
                 header = 'Content-Encoding: ' + r.headers['content-encoding']
-                issue = self._format_report('bad', 
-                    description_formats={'header': header})
+                issue = self._format_report('enabled', 
+                    description_formats={'header': header},
+                    url_list=url_list)
                 self.report_issue(issue)
             else:
-                self.report_issue(self.REPORTS['good'])
+                issue = self._format_report('disabled', url_list=url_list)
+                self.report_issue(issue)
         except minion.curly.CurlyError as error:
+            if not was_http:
+                url_list = [{'URL': target, 'Extra': 'Site not reachable'}]
+            else:
+                url_list[0]['Extra'] = 'HTTPS destination is not reachable. ' + \
+                    url_list[0]['Extra']
+            issue = self._format_report('netural', 
+                description=str(error),
+                url_list=url_list)
+            self.report_issue(issue)
+        except minion.curly.BadResponseError as error:
             self.report_issue(error.issue)
             return AbstractPlugin.EXIT_STATE_ABORTED
-        except minion.curly.BadResponseError as error:
-            issue = self._format_report('bad', description=str(error))
-            self.report_issue(issue)
-            return AbstractPlugin.EXIT_STATE_ABORTED
+
+    def _append_url(issue, urls):
+        c = 0
+        for url in urls:
+            issue['URLs'][c] = url
+            c += 1
+        return issue
+
